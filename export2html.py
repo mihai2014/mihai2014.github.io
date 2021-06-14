4fa746f2688121edb3c5d4af55f35083154a3ca5
mihai2014 <mc200520052005@yahoo.com>
2021-06-14 17:37:31 +0300

    modif: made dj template - ai.html

--- export2html.py
+++ export2html.py
@@ -8,6 +8,26 @@ toHtml.exportDir = "/home/mihai/export"
 toHtml.root = "/book"                   
 toHtml.go()
 
+headerDjangoTemplate = """
+{% load static %}
+
+{% block content %}
+
+    <div class="container">
+      <div class="row">
+
+"""
+
+footerDjangoTemplate = """
+
+      </div>
+    </div>
+
+{% endblock content %}
+"""
+
+string = headerDjangoTemplate + string + footerDjangoTemplate
+
 f = open(toHtml.exportDir + "/" + "ai.html", mode = 'w')
 f.write(toHtml.string)
 f.close()
