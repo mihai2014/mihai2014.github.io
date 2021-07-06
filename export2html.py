a1db974d9cac0ea9af0adeb3c9ffa81ab90f2d07
mihai2014 <mc200520052005@yahoo.com>
2021-07-06 18:05:04 +0300

    adding 'about'

--- export2html.py
+++ export2html.py
@@ -16,9 +16,8 @@ headerDjangoTemplate = """
 {% block content %}
 
     <div class="container">
-      <div class="row">
 
-      <!--<p style=""><a href="/book/about.html" target="_blank">About</a></p>-->
+      <div class="row">
 
 """
 
